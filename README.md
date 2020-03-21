a40b8d3e0990445fd653972e2a9742137eea1405
Tasha Hussey <latashahussey@users.noreply.github.com>
2020-03-21 17:25:38 -0500

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# skillcrush-202
-Created with CodeSandbox
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello World! My name is Tasha and I'm here to crush React skillz!</h1>
+    </div>
+  );
+}
